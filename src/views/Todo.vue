a61c5943f087bcbee41ade5128b85facdc3389e5
WadaTakafumi <wdtk2525@gmail.com>
2021-01-29 16:03:16 +0900

    Todoテンプレ追加

---
+++ src/views/Todo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="todo-section">
+    <h1>Todoリスト</h1>
+    <div class="input-field">
+      <input type="text" v-model="inputText" />
+      <button v-on:click="addTodo">追加</button>
+    </div>
+    <div class="todo-container">
+      <div v-for="todo in todos" v-bind:key="'todo' + todo.id">
+        <input type="checkbox" v-model="todo.isdone" />
+        <span>{{ todo.title }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+  data() {
+    return {
+      inputText: "",
+      todos: []
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({ title: this.inputText, isdone: false });
+      this.inputText = "";
+    }
+  }
+};
+</script>
+
+<style></style>
